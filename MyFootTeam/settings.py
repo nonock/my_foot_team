dde8e301c99246ae3b41a6afbe07945092b79495
Yannick Ezvan <yannick@ezvan.fr>
2022-11-02 17:00:19 +0100

    setup db

--- MyFootTeam/settings.py
+++ MyFootTeam/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+ENV = environ.Env()
+environ.Env.read_env()
+
+
+SECRET_KEY = ENV("SECRET_KEY")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)v9=)%b6xof%&+ro(e7lc08vvmvi%)8d68*_rb6y!50mac0k!q"
+SECRET_KEY = ENV("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +83,12 @@ WSGI_APPLICATION = "MyFootTeam.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": ENV("DB_NAME"),
+        "USER": ENV("DB_USER"),
+        "PASSWORD": ENV("DB_PASSWORD"),
+        "HOST": ENV("DB_HOST"),
+        "PORT": ENV("DB_PORT"),
     }
 }
 
